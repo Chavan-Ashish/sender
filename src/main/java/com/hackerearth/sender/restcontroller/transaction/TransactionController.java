fae3c7c6ec3517467604255abfb10ee8db998e04
Chavan-Ashish <47237506+Chavan-Ashish@users.noreply.github.com>
2022-04-08 20:49:13 +0530

    Update TransactionController.java

--- src/main/java/com/hackerearth/sender/restcontroller/transaction/TransactionController.java
+++ src/main/java/com/hackerearth/sender/restcontroller/transaction/TransactionController.java
@@ -14,6 +14,7 @@ import com.fasterxml.jackson.databind.JsonMappingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.hackerearth.sender.model.transaction.Transaction;
 import com.hackerearth.sender.utility.EncryptionUtility;
+import com.hackerearth.sender.utility.RESTUtility;
 
 @RestController
 @RequestMapping("/transaction/")
@@ -22,17 +23,15 @@ public class TransactionController {
 	@PostMapping("/add")
 	public JSONObject addTransaction(@RequestBody Transaction transaction) throws JsonMappingException, JsonProcessingException {
 		JSONObject obj = new JSONObject();
-		
-		Map hm = new HashMap<String, Transaction>();
-		hm.put("aa", transaction);
-		JSONObject obj2 = new JSONObject(hm);
-		String s = EncryptionUtility.encrypt(obj2.toString());
-		String a = EncryptionUtility.decrypt(s);
-		JSONObject kk = new JSONObject(a);
-		System.out.println();
-		 ObjectMapper mapper = new ObjectMapper();
-		 Transaction readValue = mapper.readValue(kk.get("aa").toString(), Transaction.class);
-	        System.out.println("readValue = " + readValue.getAccountForm());
+		Map<String,Transaction> hm = new HashMap<>();
+		hm.put("transaction", transaction);
+		String s = EncryptionUtility.encrypt(new JSONObject(hm).toString());
+//		String a = EncryptionUtility.decrypt(s);
+//		JSONObject kk = new JSONObject(a);
+//		System.out.println();
+//		 ObjectMapper mapper = new ObjectMapper();
+//		 Transaction readValue = mapper.readValue(kk.get("aa").toString(), Transaction.class);
+//	        System.out.println("readValue = " + readValue.getAccountForm());
 		return new JSONObject();
 	}
 	
